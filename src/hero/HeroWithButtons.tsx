c0e685305c4f0e89163c32886625e766a8e502d7
muffafa <mm.savar@gmail.com>
2023-09-10 12:36:24 +0300

    [update]: HeroOneButton renamed to HeroWithButtons

--- src/hero/HeroWithButtons.tsx
+++ src/hero/HeroWithButtons.tsx
@@ -1,20 +1,19 @@
 import type { ReactNode } from 'react';
 
-type IHeroOneButtonProps = {
+type IHeroWithButtonsProps = {
   title: ReactNode;
   description: string;
-  button: ReactNode;
+  buttons: ReactNode;
 };
 
-const HeroOneButton = (props: IHeroOneButtonProps) => (
+const HeroWithButtons = (props: IHeroWithButtonsProps) => (
   <header className="text-center">
     <h1 className="whitespace-pre-line text-5xl font-bold leading-hero text-gray-900">
       {props.title}
     </h1>
     <div className="mb-16 mt-4 text-2xl">{props.description}</div>
-
-    {props.button}
+    <div className="flex justify-center">{props.buttons}</div>
   </header>
 );
 
-export { HeroOneButton };
+export { HeroWithButtons };
